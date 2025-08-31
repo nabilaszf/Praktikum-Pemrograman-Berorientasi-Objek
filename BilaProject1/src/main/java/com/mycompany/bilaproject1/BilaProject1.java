a7b0e36a4db7e5c65a060af245d75026928f0093
nabilaszf <nabilaszf@gmail.com>
2025-08-31 15:21:32 +0700

    Add files via upload

---
+++ BilaProject1/src/main/java/com/mycompany/bilaproject1/BilaProject1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bilaproject1;
+
+/**
+ *
+ * @author ACER
+ */
+public class BilaProject1 {
+
+    public static void main(String[] args) {
+        System.out.println("Nama     : Nabila Syafiqah Zahran Firlina");
+        System.out.println("NPM      : 2430506058");
+        System.out.println("Alamat   : Jalan Dieng KM 5");
+        System.out.println("No HP    : 081234567898");
+    }
+}
