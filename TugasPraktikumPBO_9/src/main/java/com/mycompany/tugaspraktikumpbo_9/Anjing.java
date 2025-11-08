11a5b732a3d9ff7aecec7d22d5e8db19588ad571
nabilaszf <nabilaszf@gmail.com>
2025-11-08 11:37:39 +0700

    Add files via upload

---
+++ TugasPraktikumPBO_9/src/main/java/com/mycompany/tugaspraktikumpbo_9/Anjing.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.tugaspraktikumpbo_9;
+
+/**
+ *
+ * @author ACER
+ */
+class Anjing extends Hewan {
+    @Override
+    void suara() {
+        System.out.println("Anjing mengeluarkan suara 'Guk Guk'.");
+    }
+}
---
+++ TugasPraktikumPBO_9/src/main/java/com/mycompany/tugaspraktikumpbo_9/TugasPraktikumPBO_9.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tugaspraktikumpbo_9;
+
+/**
+ *
+ * @author ACER
+ */
+public class TugasPraktikumPBO_9 {
+
+    public static void main(String[] args) {
+        Hewan kucing = new Kucing();
+        Hewan anjing = new Anjing();
+
+        kucing.suara();
+        anjing.suara();
+    }
+}
